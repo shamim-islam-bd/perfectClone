bca9f45acd00b93472a1f8d4d94a20494e666575
Shamimzz <shamimislam5091@gmail.com>
2023-04-16 13:56:53 +0600

    a

--- components/Footer/FooterBottm.tsx
+++ components/Footer/FooterBottm.tsx
@@ -4,17 +4,25 @@ import Github from "@/public/assets/Github";
 import Linkdin from "@/public/assets/Linkdin";
 import Twiteer from "@/public/assets/Twiteer";
 import Youtube from "@/public/assets/Youtube";
-import Laila from "../../public/images/laila.png";
+// import Laila from "../../public/images/laila.png";
 
 export default function FooterBottm() {
   return (
     <div className="text-[#fff] opacity-90">
       <div className="flex font-input px-[60px] text-sm p-6 bg-[#073FF9] mb-4 flex-col lg:flex-row justify-between ">
         <ul className="flex gap-8">
-          <li><a href="#">CLOUD STATUS</a></li>
-          <li><a href="#">PRIVACY & TERMS</a></li>
-          <li><a href="#">SECURITY</a></li>
-          <li><a href="#">CONTACT US</a></li>
+          <li>
+            <a href="#">CLOUD STATUS</a>
+          </li>
+          <li>
+            <a href="#">PRIVACY & TERMS</a>
+          </li>
+          <li>
+            <a href="#">SECURITY</a>
+          </li>
+          <li>
+            <a href="#">CONTACT US</a>
+          </li>
         </ul>
         <ul className="flex gap-4">
           <li>
@@ -45,7 +53,7 @@ export default function FooterBottm() {
       </div>
       <div className="flex py-6 text-sm font-input justify-between px-[60px] items-center">
         <p>© Copyright 2023 Prefect Technologies, Inc. All rights reserved.</p>
-        <Image src={Laila} width={90} height={90} alt="" />
+        {/* <Image src={Laila} width={90} height={90} alt="" /> */}
       </div>
     </div>
   );
